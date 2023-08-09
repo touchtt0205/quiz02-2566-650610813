0600298823b01623f93d2085f08dacfa9db0f8fd
Chayanin <arm.crazyplay@gmail.com>
2023-08-09 10:05:19 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Quiz-02 : Status Post
+
+หลังจากการ Fork และ Clone repository แล้ว, ให้เปิดโฟลเดอร์ด้วย VSCode และรันคำสั่งใน terminal:
+
+```bash
+npm install
+npm run dev
+```
+
+ให้นักศึกษาสร้างหน้าเว็บจำลอง Status Post โดยใช้ความรู้เรื่อง React
+
+[คลิกเพื่อดูรายละเอียด](https://o365cmu-my.sharepoint.com/:b:/g/personal/chayanin_s_cmu_ac_th1/EdAsktfgcB5KqQqyvAZS6v8B9AmZDGkcHjocoPD51ZueRA?e=dQaMb1)
+
+หลังจาก Deploy ด้วย Vercel แล้วกรอกข้อมูลลงใน Google Form
+
+[คลิกเพื่อเปิด Google Form](https://forms.gle/NtW3R2gfyUZuAX217)
---
+++ src/app/page.js
@@ -0,0 +1,94 @@
+"use client";
+
+export default function HomePage() {
+  return (
+    <div
+      style={{ minHeight: "100vh", backgroundColor: "#18191A" }}
+      className="p-3"
+    >
+      {/* Entire App Container */}
+      <div
+        style={{ maxWidth: "700px", backgroundColor: "#242526" }}
+        className="mx-auto p-3 rounded rounded-3 shadow-sm"
+      >
+        {/* Post Owner Example*/}
+        <div className="vstack gap-3">
+          <div className="d-flex align-items-center gap-3">
+            <img
+              src="/profileImages/handsome.jpg"
+              width="48"
+              height="48"
+              className="rounded-circle"
+              style={{ objectFit: "cover" }}
+            />
+            <span className="fw-semibold fs-5 text-white">
+              Chayanin Suatap 650610560
+            </span>
+          </div>
+
+          <span className="text-white">
+            Quiz ง่ายจังเลยครับ ขอยาก ๆ กว่านี้ได้ไหม #261207
+          </span>
+
+          <div className="d-flex align-items-center gap-1">
+            <img src="/like.svg" width={20}></img>
+            <span style={{ color: "#B0B3B8" }}>100 คน</span>
+          </div>
+          <hr className="m-0 border" />
+        </div>
+
+        {/* Comment Example */}
+        <div className="d-flex gap-2 my-2">
+          <img
+            src="/profileImages/lisa.jpg"
+            width="48"
+            height="48"
+            className="rounded-circle"
+            style={{ objectFit: "cover" }}
+          />
+          <div
+            className="rounded rounded-3 p-2"
+            style={{ backgroundColor: "#3A3B3C" }}
+          >
+            <span className="fw-semibold" style={{ color: "#E4E6EB" }}>
+              Lisa
+            </span>
+            <br />
+            <span style={{ color: "#E4E6EB" }}>จริงค่า</span>
+            <div className="d-flex align-items-center gap-1">
+              <img src="/like.svg" width={20}></img>
+              <span style={{ color: "#B0B3B8" }}>999 คน</span>
+            </div>
+          </div>
+        </div>
+
+        {/* Reply Example */}
+        <div className="d-flex gap-2 my-2 ps-5">
+          <img
+            src="/profileImages/puppy.jpg"
+            width="48"
+            height="48"
+            className="rounded-circle"
+            style={{ objectFit: "cover" }}
+          />
+          <div
+            className="rounded rounded-3 p-2"
+            style={{ backgroundColor: "#3A3B3C" }}
+          >
+            <span className="fw-semibold" style={{ color: "#E4E6EB" }}>
+              หมาน้อย
+            </span>
+            <br />
+            <span style={{ color: "#E4E6EB" }}>จริงค้าบบบบบบบบ</span>
+            <div className="d-flex align-items-center gap-1">
+              <img src="/like.svg" width={20}></img>
+              <span style={{ color: "#B0B3B8" }}>2 คน</span>
+            </div>
+          </div>
+        </div>
+
+        {/* map-loop render Comment component here */}
+      </div>
+    </div>
+  );
+}
